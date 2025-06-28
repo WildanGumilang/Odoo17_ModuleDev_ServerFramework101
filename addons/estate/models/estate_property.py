52afb153a1c1f8f985042c004869ef6fa608a620
Muhammad Wildan Gumilang <muhammadgumilang19@gmail.com>
2025-06-28 15:55:07 +0700

    chapter 5 : Finally, Some UI To Play With

--- addons/estate/models/estate_property.py
+++ addons/estate/models/estate_property.py
@@ -1,3 +1,4 @@
+from dateutil.relativedelta import relativedelta
 from odoo import fields, models
 
 
@@ -8,10 +9,10 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True, string="Name")
     description = fields.Text(string="Description")
     postcode = fields.Char(string="Postcode")
-    date_availability = fields.Date(string="Available From")
+    date_availability = fields.Date(string="Available From", copy=False, default=lambda self: fields.Datetime.now() + relativedelta(months=3))
     expected_price = fields.Float(required=True, string="Expected Price")
-    selling_price = fields.Float(string="Selling Price")
-    bedrooms = fields.Integer(string="Bedrooms")
+    selling_price = fields.Float(string="Selling Price", readonly=True, copy=False)
+    bedrooms = fields.Integer(string="Bedrooms", default=2)
     living_area = fields.Integer(string="Living Area (sqm)")
     facades = fields.Integer(string="Number of Facades")
     garage = fields.Boolean(string="Has Garage")
@@ -26,3 +27,15 @@ class EstateProperty(models.Model):
         ],
         string="Garden Orientation"
     )
+    active = fields.Boolean(default=False)
+    state = fields.Selection(
+        [('new', 'New'),
+         ('offer_received', 'Offer Received'),
+         ('offer_accepted', 'Offer Accepted'),
+         ('sold', 'Sold'),
+         ('canceled', 'Canceled')],
+        required=True,
+        copy=False,
+        default='new',
+    )
+
