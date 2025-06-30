d72a08e7bc529c9b1addc71c2a2e4da1771e638b
Muhammad Wildan Gumilang <muhammadgumilang19@gmail.com>
2025-06-30 15:09:45 +0700

    chapter 12 : Inheritance

--- addons/estate/__manifest__.py
+++ addons/estate/__manifest__.py
@@ -4,6 +4,7 @@
         'base'
     ],
     'data': [
+        'views/res_users_views.xml',
         'views/estate_property_views.xml',
         'views/estate_property_tag_views.xml',
         'views/estate_property_offer_views.xml',
